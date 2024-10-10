7b2cb2abb47275235867983dca62dbd1ed42f1f2
nakyeonko3 <badaclock@gmail.com>
2024-10-10 14:02:26 +0900

    settings: server/ 폴더 세팅
    - express 서버 세팅
    - 프론트의 eslint 설정은 서버에도 적용됨.
    - 서버의 eslint 설정은 서버에만 적용됨.

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,55 +1,53 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
     es2021: true,
     node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'react-hooks', 'import', 'prettier'],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    'react/react-in-jsx-scope': 'off',
     'import/no-unresolved': [
       'error',
       {
-        ignore: ['^/'],
+        ignore: ['^@/'],
       },
     ],
   },
-  ignorePatterns: ['node_modules/', 'build/', 'dist/', '.github', '.tmp', 'index.html'],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.github',
+    '.tmp',
+    'server',
+    '../node_modules',
+    '../dist',
+    '../server',
+  ],
   settings: {
-    react: {
-      version: 'detect',
-    },
     'import/resolver': {
       typescript: {
         project: './tsconfig.json',
       },
       node: {
-        extensions: ['.js', '.ts', '.tsx'],
+        extensions: ['.js', '.ts'],
         paths: ['src'],
       },
       alias: {
-        map: [
-          ['@', './src'],
-          ['/', './public'],
-        ],
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.svg'],
+        map: [['@', './src']],
+        extensions: ['.ts', '.js', '.json'],
       },
     },
   },
