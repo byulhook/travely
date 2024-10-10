c289e3e330002dde1a6e9aa9454b9f357d533a35
Haeun <haruyam15@gmail.com>
2024-10-10 16:05:43 +0900

    design: 카드컴포넌트 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import CardWrap from '@/components/home/CardWrap';
+import TagCardWrap from '@/components/sub/TagCardWrap';
+import { css } from '@emotion/react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div css={sample}>
+      <TagCardWrap />
+      <CardWrap />
+    </div>
   );
 }
 
 export default App;
+
+const sample = css`
+  width: 1080px;
+  margin: 0 auto;
+`;
---
+++ src/types/tagType.ts
@@ -0,0 +1,11 @@
+export type TagType =
+  | 'Food'
+  | 'Culture'
+  | 'Healing'
+  | 'Nature'
+  | 'Sports'
+  | 'Festival'
+  | 'K-POP'
+  | 'K-DRAMA'
+  | 'JEJU'
+  | 'etc.';
