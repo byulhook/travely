c1c5e4caecca549786e181c2f1768c369d82abdf
nakyeonko3 <badaclock@gmail.com>
2024-10-11 21:31:36 +0900

    design: header 높이 및 position 변경(#2)

--- src/layouts/ContainerLayout.tsx
+++ src/layouts/ContainerLayout.tsx
@@ -1,28 +1,12 @@
 import { css, SerializedStyles } from '@emotion/react';
 
 import Main from '@/layouts/Main';
+import Header from '@/layouts/Header';
 
 interface ContainerLayoutProps {
   customStyle?: SerializedStyles;
   mainCustomStyle?: SerializedStyles;
 }
-
-const Header = () => (
-  // 테스트용 헤더 컴포넌트
-  <header
-    css={css`
-      position: fixed;
-      top: 0;
-      left: 0;
-      background-color: black; /* 테스트용 헤더 배경색 */
-      height: 70px; /* 테스트용 헤더 높이 */
-      width: 100%;
-      color: white;
-    `}
-  >
-    <h1>테스트용 헤더</h1>
-  </header>
-);
 
 const ContainerLayout: React.FC = ({ customStyle, mainCustomStyle }: ContainerLayoutProps) => {
   return (
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,3 +1,4 @@
+import theme from '@/styles/theme';
 import { css, SerializedStyles } from '@emotion/react';
 import { Outlet } from 'react-router-dom';
 
@@ -17,7 +18,7 @@ const Main: React.FC<RootLayoutProps> = ({ customStyle }) => {
 
 export const mainStyle = css`
   min-height: 100vh;
-  padding-top: 70px; /* 테스트용 헤더 높이만큼 여백 추가 */
+  padding-top: ${theme.height.header}; /* 테스트용 헤더 높이만큼 여백 추가 */
   background-color: #f5f5f5;
 `;
 export default Main;
