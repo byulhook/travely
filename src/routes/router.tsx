d8f1b72531799cbf9edc8e6742511866ccf88990
ssumanlife <ksm9805@naver.com>
2024-10-14 14:42:13 +0900

    design/충돌해결

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -7,7 +7,6 @@ import TravelList from '@/pages/TravelList';
 import AddTravel from '@/pages/AddTravel';
 import MyPageContainerLayout from '@/components/myPage/MyPageContainer';
 import AddTravel from '@/pages/AddTravel';
-import MyPageContainerLayout from '@/components/myPage/MyPageContainer';
 
 const PATH = {
   HOME: '/',
