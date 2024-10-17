cedba60ce4324253c17e706ed0d54e82ed66c913
Sonseongoh <nobleser@naver.com>
2024-10-17 16:17:14 +0900

    fix: mongoose 주석처리2 (#70)

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,68 +1,68 @@
-// import mongoose from 'mongoose';
+// // import mongoose from 'mongoose';
 
-// 여행 테이블
-export interface ITravel {
-  id: string;
-  userId: string;
-  thumbnail: string;
-  travelTitle: string;
-  travelContent: object;
-  tag: string[];
-  travelCourse: string[];
-  includedItems?: string[];
-  excludedItems?: string[];
-  meetingTime?: string[];
-  meetingLocation?: object;
-  travelPrice: number;
-  travelFAQ?: object[];
-  reviews?: string[];
-  bookmark?: string[];
-  createAt: Date;
-  updateAt: Date;
-  teamId?: string[];
-  travelTotalScore?: number;
-  travelActive: boolean;
-}
+// // 여행 테이블
+// export interface ITravel {
+//   id: string;
+//   userId: string;
+//   thumbnail: string;
+//   travelTitle: string;
+//   travelContent: object;
+//   tag: string[];
+//   travelCourse: string[];
+//   includedItems?: string[];
+//   excludedItems?: string[];
+//   meetingTime?: string[];
+//   meetingLocation?: object;
+//   travelPrice: number;
+//   travelFAQ?: object[];
+//   reviews?: string[];
+//   bookmark?: string[];
+//   createAt: Date;
+//   updateAt: Date;
+//   teamId?: string[];
+//   travelTotalScore?: number;
+//   travelActive: boolean;
+// }
 
-// 리뷰 테이블
-export interface IReview {
-  id: string;
-  userId: string;
-  travelId: string;
-  reviewImg?: string[];
-  createdDate: Date;
-  content: string;
-  travelScore: number;
-}
+// // 리뷰 테이블
+// export interface IReview {
+//   id: string;
+//   userId: string;
+//   travelId: string;
+//   reviewImg?: string[];
+//   createdDate: Date;
+//   content: string;
+//   travelScore: number;
+// }
 
-// 신청한 유저 테이블
-export interface IAppliedUser {
-  userId: string;
-  appliedAt: Date;
-  status: 'waiting' | 'approved' | 'rejected';
-}
+// // 신청한 유저 테이블
+// export interface IAppliedUser {
+//   userId: string;
+//   appliedAt: Date;
+//   status: 'waiting' | 'approved' | 'rejected';
+// }
 
-// 팀 테이블
-export interface ITeam {
-  id: string;
-  travelId: string;
-  personLimit: number;
-  appliedUsers: IAppliedUser[];
-  travelStartDate: Date;
-  travelEndDate: Date;
-}
+// // 팀 테이블
+// export interface ITeam {
+//   id: string;
+//   travelId: string;
+//   personLimit: number;
+//   appliedUsers: IAppliedUser[];
+//   travelStartDate: Date;
+//   travelEndDate: Date;
+// }
 
-// 유저 테이블
-export interface IUser {
-  id: string;
-  userProfileImage: string;
-  userName: string;
-  userId: string;
-  userEmail: string;
-  phoneNumber: string;
-  mbti: string;
-  myCreatedTravel: string[];
-  myPassedTravel: string[];
-  myReviews: string[];
-  myBookmark: mongoose.Types.ObjectId[];
-}
+// // 유저 테이블
+// export interface IUser {
+//   id: string;
+//   userProfileImage: string;
+//   userName: string;
+//   userId: string;
+//   userEmail: string;
+//   phoneNumber: string;
+//   mbti: string;
+//   myCreatedTravel: string[];
+//   myPassedTravel: string[];
+//   myReviews: string[];
+//   myBookmark: mongoose.Types.ObjectId[];
+// }
