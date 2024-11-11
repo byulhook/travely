c661bfd985a1841608adafd3b26ee9d9469fbd33
Haeun <haruyam15@gmail.com>
2024-11-11 20:13:25 +0900

    feat: api주석처리 후 홈 목데이터로 교체(#94)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,46 +2,47 @@ import TagCardWrap from '@/components/TagCardWrap';
 import TravelCard from '@/components/TravelCard';
 import travelCardMockData from '@/data/travelCardMockData';
 import { css } from '@emotion/react';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import { useQuery } from '@tanstack/react-query';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const mockDatas = travelCardMockData;
 
 const Home = () => {
-  const userId = 'user001';
-  const [cardDatas, setCardDatas] = useState(null);
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['home-travel-list'],
-    queryFn: () => fetchHomeTravelList(userId),
-  });
-  const fetchHomeTravelList = async (userId: string) => {
-    try {
-      const res = await axios.post('http://3.37.101.147:3000/api/v1/travels/home-travel-list', {
-        userId,
-      });
-      return res.data.data.travels;
-    } catch (error) {
-      console.error('여행 목록을 조회하는데 실패했습니다: ' + error);
-      throw error;
-    }
-  };
-  useEffect(() => {
-    if (data) {
-      setCardDatas(data);
-    }
-  }, [data]);
+  //api 수정되면 수정예정입니다 주석 삭제하지 말아주세요.
+  // const user = useUserStore((state)=>state.user)
+  // // const [cardDatas, setCardDatas] = useState(null);
+  // // const { data, isLoading, isError } = useQuery({
+  // //   queryKey: ['home-travel-list'],
+  // //   queryFn: () => fetchHomeTravelList(userId),
+  // // });
+  // // const fetchHomeTravelList = async (userId: string) => {
+  // //   try {
+  // //     const res = await axios.post('http://3.37.101.147:3000/api/v1/travels/home-travel-list', {
+  // //       userId,
+  // //     });
+  // //     return res.data.data.travels;
+  // //   } catch (error) {
+  // //     console.error('여행 목록을 조회하는데 실패했습니다: ' + error);
+  // //     throw error;
+  // //   }
+  // // };
+  // // useEffect(() => {
+  // //   if (data) {
+  // //     setCardDatas(data);
+  // //   }
+  // // }, [data]);
 
-  if (isLoading) {
-    return <p>loading...</p>;
-  }
+  // // if (isLoading) {
+  // //   return <p>loading...</p>;
+  // // }
 
-  if (isError) {
-    return null;
-  }
+  // // if (isError) {
+  // //   return null;
+  // // }
 
-  console.log(cardDatas);
+  // console.log(cardDatas);
   return (
     <div css={home}>
       <TagCardWrap shape="square" />
